6db1f812dc25da98b2155cf1a8c36ffec8f3ebd7
Buddhamon <wlacey@smu.edu>
2022-05-31 12:00:39 -0700

    Updated Package to v0.0.12
    * added Rotator class for rotating a transform
    * fixed Singleton class calling DestroyImmediate() on current instance multiple Set() occurred

---
+++ Runtime/Scripts/Additional/Rotator.cs
@@ -0,0 +1,35 @@
+/*
+ * File Name: Rotator.cs
+ * Description: This script is for rotating a transform
+ * 
+ * Author(s): Kokowolo, Will Lacey
+ * Date Created: May 13, 2022
+ * 
+ * Additional Comments:
+ *		File Line Length: 120
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    /************************************************************/
+    #region Fields
+
+    [Tooltip("speed to rotate transform's euler angles")]
+    [SerializeField] Vector3 speed = new Vector3(0, 50, 0);
+
+    #endregion
+    /************************************************************/
+    #region Functions
+
+    private void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime);
+    }
+
+    #endregion
+    /************************************************************/
+}

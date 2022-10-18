b57d9b11dd06f99ab17abf9ade074b70deedb273
Buddhamon <wlacey@smu.edu>
2022-10-18 19:49:29 -0400

    Update InputManager.cs

--- Runtime/Scripts/Managers/InputManager.cs
+++ Runtime/Scripts/Managers/InputManager.cs
@@ -14,61 +14,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-using Kokowolo.Utilities;
-
 #if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
 #define USE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
 #endif
 
-[DefaultExecutionOrder(-100)]
-public class InputManager : MonoBehaviour
+namespace Kokowolo.Utilities
 {
-	/************************************************************/
-    #region Properties
-
-    protected static InputManager Instance => Singleton<InputManager>.Get();
-
-    #endregion
-    /************************************************************/
-    #region Functions
-
-    protected virtual void Awake()
+    [DefaultExecutionOrder(-100)]
+    public class InputManager : MonoBehaviour
     {
-        Singleton<InputManager>.Set(this);
-    }
+        /************************************************************/
+        #region Properties
 
-    public static bool IsMouseOverUI()
-    {
-        return EventSystem.current.IsPointerOverGameObject();
-    }
+        protected static InputManager Instance => Singleton<InputManager>.Get();
 
-    public static Vector2 GetMouseScreenPoint()
-    {
-#if USE_NEW_INPUT_SYSTEM
-        return Mouse.current.position.ReadValue();
-#else
-        return Input.mousePosition;
-#endif
-    }
+        #endregion
+        /************************************************************/
+        #region Functions
 
-    public static Vector3 GetMouseWorldPosition(LayerMask layerMask, 
-        float maxDistance = Mathf.Infinity, Camera camera = null)
-    {
-        Raycasting.RaycastFromMouseScreenPoint(out RaycastHit raycastHit, layerMask, maxDistance, camera);
-        return raycastHit.point;
-    }
+        protected virtual void Awake()
+        {
+            Singleton<InputManager>.Set(this);
+        }
 
-    public static bool WasClickPressedThisFrame() => Instance._WasClickPressedThisFrame();
-    protected virtual bool _WasClickPressedThisFrame()
-    {
-#if USE_NEW_INPUT_SYSTEM
-        return Mouse.current.leftButton.wasPressedThisFrame;
-#else
-        return Input.GetMouseButtonDown(0);
-#endif
+        public static bool IsMouseOverUI()
+        {
+            return EventSystem.current.IsPointerOverGameObject();
+        }
+
+        public static Vector2 GetMouseScreenPoint()
+        {
+    #if USE_NEW_INPUT_SYSTEM
+            return Mouse.current.position.ReadValue();
+    #else
+            return Input.mousePosition;
+    #endif
+        }
+
+        public static Vector3 GetMouseWorldPosition(LayerMask layerMask, 
+            float maxDistance = Mathf.Infinity, Camera camera = null)
+        {
+            Raycasting.RaycastFromMouseScreenPoint(out RaycastHit raycastHit, layerMask, maxDistance, camera);
+            return raycastHit.point;
+        }
+
+        public static bool WasClickPressedThisFrame() => Instance._WasClickPressedThisFrame();
+        protected virtual bool _WasClickPressedThisFrame()
+        {
+    #if USE_NEW_INPUT_SYSTEM
+            return Mouse.current.leftButton.wasPressedThisFrame;
+    #else
+            return Input.GetMouseButtonDown(0);
+    #endif
+        }
+        
+        #endregion
+        /************************************************************/
     }
-    
-    #endregion
-    /************************************************************/
 }

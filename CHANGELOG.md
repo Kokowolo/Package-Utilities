448cae92b61395ef03466ffffcdf3895087089b5
Wollbobaggins <wlacey@smu.edu>
2023-01-25 03:11:47 -0500

    Updated Package to v0.0.30
    * added setting field to DontDestroyOnLoadScript to determine whether only one DontDestroyOnLoadScript GameObject can exist with its name
    * changed DontDestroyOnLoadScript to track unique singleton instance DontDestroyOnLoadScript GameObjects

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.30] - 2023-01-25
+### Added
+* setting field to DontDestroyOnLoadScript to determine whether only one DontDestroyOnLoadScript GameObject can exist with its name
+### Changed
+* DontDestroyOnLoadScript to track unique singleton instance DontDestroyOnLoadScript GameObjects
+
 ## [0.0.29] - 2023-01-24
 ### Added
 * a simple DontDestroyOnLoadScript to Components folder
--- Runtime/Scripts/Components/DontDestroyOnLoadScript.cs
+++ Runtime/Scripts/Components/DontDestroyOnLoadScript.cs
@@ -7,6 +7,8 @@
  * 
  * Additional Comments:
  *		File Line Length: 120
+ * 
+ *      NOTE: DontDestroyOnLoadScript requires the MonoBehaviour's GameObject to have a unique name
  */
 
 using System.Collections;
@@ -18,11 +20,34 @@ namespace Kokowolo.Utilities
     public class DontDestroyOnLoadScript : MonoBehaviour
     {
         /************************************************************/
+        #region Fields
+
+        [Tooltip("whether only one DontDestroyOnLoadScript GameObject can exist with this name")]
+        [SerializeField] private bool isSingletonInstance = true;
+
+        private static List<string> singletonInstanceNames = new List<string>();
+
+        #endregion
+        /************************************************************/
         #region Functions
+
+        private void Awake()
+        {
+            if (isSingletonInstance && HasSingletonInstanceWithName(name)) 
+            {
+                Destroy(gameObject);
+            }
+        }
 
         private void Start()
         {
             DontDestroyOnLoad(this);
+            if (isSingletonInstance) singletonInstanceNames.Add(name);
+        }
+
+        private static bool HasSingletonInstanceWithName(string name)
+        {
+            return singletonInstanceNames.Contains(name);
         }
         
         #endregion

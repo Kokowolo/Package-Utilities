0472f553a18938263c310bead21fa4f5cffcc3c2
macOS:Wollbobaggins <wlacey@smu.edu>
2025-07-04 16:47:58 -0500

    Updated Package to v0.0.115
    * added `AnimatorExtensions` along with its extension function `Play([...], bool prewarm)`

---
+++ Runtime/Scripts/Extensions/AnimatorExtensions.cs
@@ -0,0 +1,41 @@
+/* 
+ * Author(s): Kokowolo, Will Lacey
+ * Date Created: July 4, 2025
+ * 
+ * Additional Comments:
+ *      File Line Length: ~140
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kokowolo.Utilities
+{
+    public static class AnimatorExtensions
+    {
+        /*██████████████████████████████████████████████████████████*/
+        #region Fields
+
+        #endregion
+        /*██████████████████████████████████████████████████████████*/
+        #region Properties
+
+        #endregion
+        /*██████████████████████████████████████████████████████████*/
+        #region Functions
+        
+        public static void Play(this Animator animator, int layer, bool prewarm)
+        {
+            animator.Play(0, layer, prewarm ? UnityEngine.Random.Range(0f, 1f) : 0);
+        }
+
+        public static void PlayPrewarmed(this Animator animator, string stateName, int layer, bool prewarm)
+        {
+            animator.Play(stateName, layer, prewarm ? UnityEngine.Random.Range(0f, 1f) : 0);
+        }
+
+        #endregion
+        /*██████████████████████████████████████████████████████████*/
+    }
+}
